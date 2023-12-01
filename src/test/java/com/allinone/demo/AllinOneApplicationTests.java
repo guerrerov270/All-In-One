c8212e4a7b626a2d97559a78ae33115dd99861a8
Julián Guerrero <julian.guerrero@keyrus.com>
2023-12-01 15:46:28 -0500

    Initial commit

---
+++ src/test/java/com/allinone/demo/AllinOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allinone.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllinOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
