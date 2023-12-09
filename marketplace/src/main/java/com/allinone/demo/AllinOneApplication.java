c9f55791edeb76f99df45d2541ed4bdf584f9c73
Julián Guerrero <julian.guerrero@keyrus.com>
2023-12-09 11:01:05 -0500

    Revert "Fixing redis config"
    This reverts commit c605a13ab6a5a988019461f76864a7c378122907.

--- marketplace/src/main/java/com/allinone/demo/AllinOneApplication.java
+++ marketplace/src/main/java/com/allinone/demo/AllinOneApplication.java
@@ -2,10 +2,9 @@ package com.allinone.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableCaching
 public class AllinOneApplication {
 
 	public static void main(String[] args) {
